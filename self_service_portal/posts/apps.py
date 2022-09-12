a2dd7e5960d05e83a4958c0e995457d038596f54
squeeze1337 <mail2hamann@gmail.com>
2022-09-12 16:25:37 +0200

    Configured posts app

--- self_service_portal/posts/apps.py
+++ self_service_portal/posts/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posts'
+    name = 'self_service_portal.posts'
+    verbose_name = "Johanniter Posts"

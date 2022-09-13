ab0297a5241b08fa3fcc14079bb3c4d22f12fa99
squeeze1337 <mail2hamann@gmail.com>
2022-09-13 11:30:41 +0200

    Configured admin for post app

--- self_service_portal/posts/admin.py
+++ self_service_portal/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from self_service_portal.posts.models import Post, PostCategory
+
+
+@admin.register(PostCategory)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ["slug", "published_on"]

42f0c69f289b18c32c0fcbcb8d50118d61ab0075
squeeze1337 <mail2hamann@gmail.com>
2022-09-14 10:54:37 +0200

    created generic class based views for post and default list view

--- self_service_portal/posts/urls.py
+++ self_service_portal/posts/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from self_service_portal.posts.views import PostCreateView
+from self_service_portal.posts.views import (
+    PostCategoryCreateView,
+    PostCreateView,
+    PostListView,
+)
 
 app_name = "posts"
 
 urlpatterns = [
     path("create/", view=PostCreateView.as_view(), name="create"),
+    path(
+        "category/create/",
+        view=PostCategoryCreateView.as_view(),
+        name="category-create",
+    ),
+    path("overview/", view=PostListView.as_view(), name="list"),
 ]
--- self_service_portal/posts/views.py
+++ self_service_portal/posts/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
-from self_service_portal.posts.models import Post
+from self_service_portal.posts.models import Post, PostCategory
 
 
 # Create your views here.
 class PostCreateView(CreateView):
     model = Post
-    fields = ["status", "title", "sub_title"]
+    fields = ["status", "title", "sub_title", "category", "conten", "image"]
+
+
+class PostCategoryCreateView(CreateView):
+    model = PostCategory
+    fields = ["title", "description"]
+
+
+class PostListView(ListView):
+    mode = Post
+    queryset = Post.objects.all()

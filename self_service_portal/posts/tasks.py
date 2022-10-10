5385e24cb252032a6c97cdcbc2c321d90cf7ffce
squeeze1337 <mail2hamann@gmail.com>
2022-10-10 16:08:16 +0200

    added async tasks and rest-api

---
+++ self_service_portal/posts/tasks.py
@@ -0,0 +1,36 @@
+from celery.utils.log import get_task_logger
+from django.core.mail import send_mail
+
+from config import celery_app
+from self_service_portal.posts.models import Post
+
+logger = get_task_logger(__name__)
+
+
+@celery_app.task()
+def send_list_of_blog_posts():
+    posts = Post.objects.all()
+    post_string = ""
+    for post in posts:
+        logger.info("Blog Post %s" % post)
+        post_string += "Post: {} \n".format(post.title)
+
+    send_mail(
+        "List of posts",
+        "Here is the list of posts\n " + post_string,
+        "from@example.org",
+        ["to@example.org"],
+        fail_silently=False,
+    )
+
+
+@celery_app.task()
+def send_marketing_report_periodically():
+    logger.info("Hello World")
+    send_mail(
+        "Marketing Report",
+        "Hello World",
+        "from@example.org",
+        ["to@example.org"],
+        fail_silently=False,
+    )
